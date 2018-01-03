452fd4bfaa4bae202e6027b5a854345a09c1cd85
aaua-sandbox <16499811+aaua-sandbox@users.noreply.github.com>
2018-01-03 20:47:00 +0900

    add pages/Counter

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,12 +2,14 @@
 import React, { Fragment } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
+import Counter from './pages/Counter'
 
 export default function Routes() {
   return (
     <Fragment>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/counter" exact component={Counter} />
       </Switch>
     </Fragment>
   )
