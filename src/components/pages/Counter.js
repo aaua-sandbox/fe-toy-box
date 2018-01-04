060246b40f0e4df1adbd07b92f4983b093ab6d53
aaua-sandbox <16499811+aaua-sandbox@users.noreply.github.com>
2018-01-04 19:08:32 +0900

    add comments

--- src/components/pages/Counter.js
+++ src/components/pages/Counter.js
@@ -3,6 +3,11 @@ import React from 'react'
 import enhancer from '../../enhancers/counterEnhancer'
 import Label from '../atoms/Label'
 
+/* 
+  props.actions.increment()でcounterEnhancerのCounterActionsで参照された
+  reducersのincrement(ActionCreators)を呼び出す.
+  incrementが呼び出されることでaction.typeにINCREMENTがセットされReducerによりstate.valueが加算される
+ */
 export default enhancer(props => {
   return (
     <div>
